dedbf76b684ccd94785551619a4b4d76f91bd642
headwire <bp.headwire@gmail.com>
2016-02-22 13:09:45 +0200

    Video for Visitka page

--- js/main.js
+++ js/main.js
@@ -355,7 +355,20 @@ $(document).ready(function(){
     $('.swipebox').swipebox();
 
     /* Video */
-    $('.swipebox-video').swipebox();
+    $('.swipebox-video').swipebox({
+        useCSS : true, // false will force the use of jQuery for animations
+        useSVG : false, // false to force the use of png for buttons
+        initialIndexOnArray : 0, // which image index to init when a array is passed
+        hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
+        hideBarsDelay : 3000, // delay before hiding bars on desktop
+        videoMaxWidth : 1140, // videos max width
+        beforeOpen: function() {}, // called before opening
+        afterOpen: null, // called after opening
+        afterClose: function() {}, // called after closing
+        loopAtEnd: false, // true will return to the first image after the last image is reached</span>
+        autoplayVideos: true, // true will autoplay Youtube and Vimeo videos
+        toggleClassOnLoad: '' // CSS class that can be toggled when the slide will be loaded (like 'hidden' of Bootstrap)
+    });
 
 
 /* ------------------------------------- */
